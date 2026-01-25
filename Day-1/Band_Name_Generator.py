292dabfa6f7cf1101ab724970461ed0f9d8d42bf
Vinayak-Burgess <VinayakBurgess+Github@gmail.com>
2026-01-25 13:38:16 +0100

    Manual upload of Day 1 Project File

---
+++ Day-1/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+A = input("Which city did you grew up in?\n")
+B = input("What is the name of your pet?\n")
+
+print("Your Band Name could be " + A + B +".")
